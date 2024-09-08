a4ef54027e6c6acb744f3bed8cc2e14f11f43ebd
AlexandreSinger <alex.singer@mail.utoronto.ca>
2024-09-08 10:41:44 -0400

    [REBASE] Removed Old AP Code and Upgraded Prepacker[REBASE] Removed Old AP Code and Upgraded Prepacker[REBASE] Removed Old AP Code and Upgraded Prepacker[REBASE] Removed Old AP Code and Upgraded Prepacker[REBASE] Removed Old AP Code and Upgraded Prepacker[REBASE] Removed Old AP Code and Upgraded Prepacker[REBASE] Removed Old AP Code and Upgraded Prepacker[REBASE] Removed Old AP Code and Upgraded Prepacker[REBASE] Removed Old AP Code and Upgraded Prepacker

--- vpr/src/place/analytical_placement/read_atom_netlist.h
+++ vpr/src/place/analytical_placement/read_atom_netlist.h
@@ -2,9 +2,9 @@
 #pragma once
 
 class AtomContext;
-class APPrepacker;
+class Prepacker;
 class UserPlaceConstraints;
 class APNetlist;
 
-APNetlist read_atom_netlist(const AtomContext& atom_ctx, const APPrepacker& prepacker, const UserPlaceConstraints& constraints);
+APNetlist read_atom_netlist(const AtomContext& atom_ctx, const Prepacker& prepacker, const UserPlaceConstraints& constraints);
 
